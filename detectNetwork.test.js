b8f8234df03a991fc07cc8d25c51124dbcecda0a
Eric Bayasgalan <ewiz1020@gmail.com>
2020-10-30 11:20:28 -0700

    Assignment Complete

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -6,41 +6,41 @@
 // You don't actually want to fill *this* value in on line 9, but you'll see
 // other places in this file where you'll replace the FILL_ME_IN with a
 // different value.
-var FILL_ME_IN = 'Fill this value in';
+// var FILL_ME_IN = 'Fill this value in';
 
-describe('Introduction to Mocha Tests - READ ME FIRST', function () {
-  // A Mocha test is just a function!
-  // If the function throws an error when run, it fails.
-  // If it doesn't throw an error when run, it doesn't fail.
-  // To read more about mocha, visit mochajs.org
+// describe('Introduction to Mocha Tests - READ ME FIRST', function() {
+// A Mocha test is just a function!
+// If the function throws an error when run, it fails.
+// If it doesn't throw an error when run, it doesn't fail.
+// To read more about mocha, visit mochajs.org
 
-  // Once you've read and understood this section, please comment it out.
-  // You will not be able to proceed with a failing test.
+// Once you've read and understood this section, please comment it out.
+// You will not be able to proceed with a failing test.
 
-  it('Throws an error so it fails', function () {
-    return 'Delete me!';
-  });
+// it('Throws an error so it fails', function() {
+//   throw new Error('Delete me!');
+// });
 
-  it('Doesn\'t throw an error, so it doesn\'t fail', function () {
-    // This test doesn't really test anything at all! It will pass no matter what.
-    var even = function (num) {
-      return num / 2 === 0;
-    };
-    return even(10) === true;
-  });
+// it('Doesn\'t throw an error, so it doesn\'t fail', function() {
+// This test doesn't really test anything at all! It will pass no matter what.
+//   var even = function(num) {
+//     return num / 2 === 0;
+//   };
+//   return even(10) === true;
+// });
 
-  // In tests, we want to compare the expected behavior to the actual behavior.
-  // A test should only fail if the expected behavior doesn't match the actual.
-  it('Throws an error when expected behavior does not match actual behavior', function () {
-    var even = function (num) {
-      return num % 2 === 0;
-    };
+// In tests, we want to compare the expected behavior to the actual behavior.
+// A test should only fail if the expected behavior doesn't match the actual.
+//   it('Throws an error when expected behavior does not match actual behavior', function() {
+//     var even = function(num) {
+//       return num / 2 === 0;
+//     };
 
-    if (even(10) !== true) {
-      throw new Error('10 should be even!');
-    }
-  });
-});
+//     if (even(10) !== true) {
+//       throw new Error('10 should be even!');
+//     }
+//   });
+// });
 describe('Diner\'s Club', function () {
   // Be careful, tests can have bugs too...
 
